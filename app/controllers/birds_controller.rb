36cea285f3a3e750fe2d5081463bb67f1074f952
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-08 21:21:12 +0300

    Added endpointsto Bird Controller.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,4 +6,19 @@ class BirdsController <  ApplicationController
         all_birds.to_json
     end
 
+    get '/birds/:id' do
+        birds_id = Bird.find(params[:id])
+        birds_id.to_json
+    end
+
+    get 'birds/:name' do
+        bird_name = Bird.find_by_name(params[:name])
+        bird_name.to_json
+    end
+
+    get 'birds/:color' do 
+        bird_color = Bird.find_by_color(params[:primary_color])
+        bird_color.to_json
+    end
+
 end
