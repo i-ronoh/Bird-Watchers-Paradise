a3a5480f54f08260ad6087fff06002058da9256c
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-11 20:59:26 +0300

    Enabled cross-origin in controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
+require 'sinatra'
+require 'sinatra/cross_origin'
+
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
 
-    get '/' do
-        "Hello World"
+    configure do
+        enable :cross_origin
+    end
+    
+    before do
+        response.headers['Access-Control-Allow-Origin'] = '*'
+    end
+      
+      # routes...
+    options "*" do
+        response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        200
     end    
 end
