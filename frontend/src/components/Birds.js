33b8248532dbf5d2460b07a20dd5f3b0ecd27aaf
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-11 21:38:20 +0300

    Included feet and beak types

--- frontend/src/components/Birds.js
+++ frontend/src/components/Birds.js
@@ -20,37 +20,37 @@ return(
             <h3 className="birds-title">THE BIRDS</h3>
         </div>
 
-        <div className="blog-container ">
+        <div className="bird-container ">
             {birds.map((bird) => (
                 <div
-                className="row "
-                style={{ marginBottom: "3em", marginTop: "3em" }}
-                key={bird.id}
+                    className="row "
+                    style={{ marginBottom: "3em", marginTop: "3em" }}
+                    key={bird.id}
                 >
-                <div className="col-md-5 ">
+                    <div className="col-md-5 ">
 
-                    <div className="card flexx" style={{ width:"500px" }}>
-                    <img
-                        src={bird.image}
-                        className="card-img-top img-fluid"
-                        alt="..."
-                    // style={{ width: "100%" }}
-                    />
-                    <div className="card-body">
-                        <h5 className="card-title">{bird.name}</h5>
-                        <h5 className="card-title">{bird.primary_color}</h5>
-                        <h5 className="card-text">{bird.beak}</h5>
-                        <h5 className="card-text">{bird.flies}</h5>
+                        <div className="card flexx" style={{ width:"500px" }}>
+                        <img
+                            src={bird.image}
+                            className="card-img-top img-fluid"
+                            alt="..."
+                        // style={{ width: "100%" }}
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">Name:{bird.name}</h5>
+                            <h5 className="card-title">Primary Color:{bird.primary_color}</h5>
+                            <p className="card-text">Beak Type: {bird.beak}</p>
+                            <p className="card-text">Feet Type: {bird.feet_type}</p>
+
+                            <p className="card-text">{bird.flies}</p>
+
+                        </div>
+                        </div>
 
                     </div>
-                    </div>
-
                 </div>
-                </div>
-
             ))}
         </div>
-      
     </>
   );
 }
