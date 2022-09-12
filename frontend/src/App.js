3e21a0411d60e9aef73a09f6298b632432936a8a
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 16:33:43 +0300

    Created Route to Gallery

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from './components/Home'
 import Navigation from './components/Navigation'
 import Birds from './components/Birds'
 import Sightings from './components/Sightings'
+import Gallery from './components/Gallery'
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
@@ -14,7 +16,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/birds' element={<Birds/>} />
-          <Route exact path='/sightings' element={<Sightings/>} />
+          <Route exact path='/sightings/:birds_id' element={<Sightings/>} />
+          <Route exact path='/gallery' element={<Gallery/>} />
 
         </Routes>
       </Router>
