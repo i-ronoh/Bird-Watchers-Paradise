061d473fea70a7e904af6b53424dfaa4d0025178
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 22:46:11 +0300

    Added Project Description to Homepage

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,9 +4,13 @@ import "./home.css"
 function Home() {
   return (
     <>
-        <div className="homeContainer striped-border">
+        <div className="home-head">
+                <h3 className="home-title">BIRD WATCHERS PARADISE</h3>
+            </div>
+        <div className="home-Container striped-border">
             <h3 className="title">...</h3>
-            <p className="description">bird watcher's paradise</p>
+            <p className="description">Spotted a bird? Identify it using our bird watchers paradise catalog.<br/>
+You can even further upload your sighting to our bird watchers paradise gallery! <br/> Enjoy!</p>
         </div>
     </>
   )
