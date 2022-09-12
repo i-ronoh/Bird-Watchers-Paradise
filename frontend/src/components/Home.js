16e0f8373dadde6d1e46a983d4f248ee1e9f998d
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 13:09:26 +0300

    Added project title and description

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./home.css"
+
+function Home() {
+  return (
+    <>
+        <div className="homeContainer striped-border">
+            <h3 className="title">...</h3>
+            <p className="description">bird watcher's paradise</p>
+        </div>
+    </>
+  )
+}
+
+export default Home;
