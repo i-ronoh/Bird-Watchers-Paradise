fe0ecd61d5730fe577841c1ab01a67179e29cae3
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 16:30:49 +0300

    Created Gallery Section to store user sightings

---
+++ frontend/src/components/Gallery.js
@@ -0,0 +1,61 @@
+import React, {useEffect, useState, useRef} from 'react';
+
+import './birds.css'
+
+function Gallery(){
+
+    const [gallery, setGallery] = useState([]) 
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:9292/sightings", {crossDomain:true,})
+        
+        .then((res) => res.json())
+        .then((data) =>{
+            console.log(data)
+            setGallery(data);
+        });
+    }, []);
+
+    
+    return(
+        <>
+            gallery 
+            {/* <div className="bird-container ">
+                {birds.map((bird) => (
+                    <div
+                        className="row "
+                        style={{ marginBottom: "3em", marginTop: "3em" }}
+                        key={bird.id}
+                    >
+                        <div className="col-md-5 ">
+
+                            <div className="card flexx" style={{ width:"500px" }}>
+                            <img
+                                src={bird.image}
+                                className="card-img-top img-fluid"
+                                alt="..."
+                            // style={{ width: "100%" }}
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">Name:{bird.name}</h5>
+                                <h5 className="card-title">Primary Color:{bird.primary_color}</h5>
+                                <p className="card-text">Beak Type: {bird.beak}</p>
+                                <p className="card-text">Feet Type: {bird.feet_type}</p>
+
+                                <p className="card-text">{bird.flies}</p>
+                                <button type="submit" className="btn btn-primary mb-3" onClick = {() =>{sighted(bird.id)}}
+                        >
+                            Seen This Bird? Click to Upload
+                        </button>
+                            </div>
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div> */}
+
+        </>
+    );
+}
+export default Gallery;
