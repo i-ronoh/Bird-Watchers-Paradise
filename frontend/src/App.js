58b7ec0813ee53df87719583d953ce96bcbd9537
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 13:10:28 +0300

    Added Home Page Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Home'
 import Navigation from './components/Navigation'
 import Birds from './components/Birds'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Navigation />
         <Routes>
-          {/* <Route exact path='/' element={<Header/>} /> */}
+          <Route exact path='/' element={<Home/>} />
           <Route exact path='/birds' element={<Birds/>} />
           {/* <Route exact path='/register' element={<Sightings/>} /> */}
 
