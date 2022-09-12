42668160e653cec91248bd2ccc034929e9190181
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 14:43:47 +0300

    Created Form to Collect Sighting Data from User

---
+++ frontend/src/components/Sightings.js
@@ -0,0 +1,103 @@
+import React, {useMemo, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./sightings.css"
+
+const Sightings = () =>{
+
+    const [sighting, setSighting] = useState({date_of_sighting: "",location_of_sighting: "", observation: "", birds_id: "", user_id: ""});
+
+    const navigate = useNavigate();
+
+    const onFieldsChange = (fieldName, value)=>{
+        setSighting({
+            ...sighting,
+            [fieldName]: value
+        });
+    }
+
+    const postSighting = (e)=>{
+        sighting.date_of_sighting = "";
+        sighting.location_of_sighting = "";
+        sighting.observation = "";
+        sighting.birds_id = "";
+        sighting.user_id = "";
+        let requestBody = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(sighting)
+        }
+        fetch("http://127.0.0.1:9292/sightings/upload", requestBody)
+            .then(response => response.json())
+            .then(data => {
+                // do the redirect here here.
+                navigate('/sighting');
+            }).catch((err)=>{
+                console.log(err);
+            });
+    }
+
+
+    // returns whether the subit button should be active
+    const submitButtonActive = useMemo(()=>(
+        sighting.date_of_sighting.trim().length !== 0 && sighting.location_of_sighting.trim().length !== 0 && sighting.observation.trim().length !== 0 && sighting.birds_id.trim().length !== 0 && sighting.user_id.trim().length !== 0
+    ), [sighting]);
+
+    return (
+        <>
+        <div className="sightings-head">
+            <h3 className="sightings-title">UPLOAD SIGHTING</h3>
+        </div>
+        <div className="container">
+            <div className="row" style={{marginTop: "5em"}}>
+                <div className="col-md-9 shadow p-5 mb-5 bg-body rounded">
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Date :</label>
+                        <input type="title" className="form-control" id="exampleFormControlInput1"
+                            onChange = {(event)=>{
+                                onFieldsChange("date_of_sighting", event.target.value)
+                            }}
+                        />
+
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Location :</label>
+                        <input type="title" className="form-control" id="exampleFormControlInput1"
+                            onChange = {(event)=>{
+                                onFieldsChange("location_of_sighting", event.target.value)
+                            }}
+                        />
+    
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlTextarea1" className="form-label">Observation :</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"
+                            onChange = {(event)=>{
+                                onFieldsChange("obdervation", event.target.value)
+                            }}
+                        ></textarea>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="exampleFormControlInput1" className="form-label">Bird ID :</label>
+                        <input type="title" className="form-control" id="exampleFormControlInput1"
+                            onChange = {(event)=>{
+                                onFieldsChange("observation", event.target.value)
+                            }}
+                        />
+    
+                    </div>
+                    <div>
+                        <button type="submit" className="btn btn-primary mb-3" disabled={!submitButtonActive}
+                            onClick={postSighting}
+                        >
+                            SUBMIT
+                        </button>
+                    </div>
+                </div>
+             </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Sightings
