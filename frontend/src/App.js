dac6988fc49d0eeb11cf9af77e7db70d75254f93
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 10:36:33 +0300

    Added Navigation Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import Navigation from './components/Navigation'
 import Birds from './components/Birds'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
-   <Birds />
-  );
-}
+
+      <Router>
+        <Navigation />
+        <Routes>
+          {/* <Route exact path='/' element={<Header/>} /> */}
+          <Route exact path='/login' element={<Birds/>} />
+          {/* <Route exact path='/register' element={<Sightings/>} /> */}
+
+        </Routes>
+      </Router>
+ 
+)}
 
 export default App;
