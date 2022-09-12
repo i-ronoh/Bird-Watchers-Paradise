a138118e088c65665ac721a931b566e268d0aee0
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 13:08:13 +0300

    Added app name to nav bar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,19 +5,26 @@ import { Link } from "react-router-dom";
 function Navigation() {
   return (
     <div className="navbar">
-      <div className = "navbar-item-right">
-        <ul className="nav-menu">
-          <li className="nav-menu-item" >
-            <Link className="link" to="/">HOME</Link>
-          </li>
-          <li className="nav-menu-item" >
-            <Link className="link"  to="/birds">BIRD LOG</Link> 
-          </li>
-          <li className="nav-menu-item">
-            <Link className="link" to="/sightings">SIGHTINGS</Link>
-          </li>
-        </ul>
-      </div>
+        <div className = "navbar-item-left">
+            <ul className="nav-menu">
+                <li className="nav-menu-item" >
+                    <Link className="link" to="/">-BWP-</Link>
+                </li>
+            </ul>
+        </div>
+        <div className = "navbar-item-right">
+            <ul className="nav-menu">
+            <li className="nav-menu-item" >
+                <Link className="link" to="/">HOME |</Link>
+            </li>
+            <li className="nav-menu-item" >
+                <Link className="link"  to="/birds">BIRD-LOG |</Link> 
+            </li>
+            <li className="nav-menu-item">
+                <Link className="link" to="/sightings">SIGHTINGS</Link>
+            </li>
+            </ul>
+        </div>
       </div>
 )};
 
