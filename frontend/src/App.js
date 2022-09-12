5d0d1dd5dab1d2910fb85e976aeb5bf70481d538
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-09-12 14:44:46 +0300

    Added Sightings Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './components/Home'
 import Navigation from './components/Navigation'
 import Birds from './components/Birds'
+import Sightings from './components/Sightings'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home/>} />
           <Route exact path='/birds' element={<Birds/>} />
-          {/* <Route exact path='/register' element={<Sightings/>} /> */}
+          <Route exact path='/sightings' element={<Sightings/>} />
 
         </Routes>
       </Router>
